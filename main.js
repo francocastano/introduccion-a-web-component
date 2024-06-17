18f53cf009f5cbc49ddff421e62679a5b7934f0c
francocastano <francomatiascastano@gmail.com>
2024-06-16 21:21:52 -0300

    Migrate to Async/Await

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-class List extends HTMLElement{
-    constructor(){
+class List extends HTMLElement {
+    constructor() {
         super();
 
-        let shadow = this.attachShadow({mode:'open'})
+        let shadow = this.attachShadow({ mode: 'open' })
 
         this.divHeader = document.createElement('div')
-        this.divContent = document.createElement("div")
-
         shadow.appendChild(this.divHeader)
+
+        this.divContent = document.createElement("div")
         shadow.appendChild(this.divContent)
     }
 
-    connectedCallback(){ 
+    async connectedCallback() {
         this.divHeader.innerHTML = this.getAttribute("data-title") || ""
-        
+
         let url = this.getAttribute("data-url")
         let field = this.getAttribute("data-field")
 
-        if (!url || !field){
+        if (!url || !field) {
             return
         }
 
         const isPicture = this.hasAttribute("modePicture")
 
-        fetch(url)
-        .then(response => response.json())
-        .then(users => users.forEach(element => {
-            this.divContent.innerHTML += isPicture ? `<img src='${element[field]}'>` : `<div>${element[field]}</div>` 
-        }));
+        const response = await fetch(url)
+        const elements = await response.json()
+
+        for (let element of elements) {
+            this.divContent.innerHTML += isPicture ? `<img src='${element[field]}'>` : `<div>- ${element[field]}</div>`
+        }
     }
 }
 
-customElements.define("app-list",List)
+customElements.define("app-list", List)
